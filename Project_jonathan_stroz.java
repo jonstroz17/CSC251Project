d89eacfaa1ad1e12703cf21005e028ea2a0bee02
Jon <jonstroz17@gmail.com>
2025-10-27 14:17:07 -0400

    Modified to read input from a file

--- Project_jonathan_stroz.java
+++ Project_jonathan_stroz.java
@@ -1,10 +1,16 @@
 import java.util.Scanner;
+import java.util.ArrayList;
+import java.io.*;
 
 public class Project_jonathan_stroz
 {
-   public static void main(String[] args)
+   public static void main(String[] args) throws IOException
    {
+      File policyFile = new File("PolicyInformation.txt");
+      Scanner fileRead = new Scanner(policyFile);
       Scanner keyboard = new Scanner(System.in);
+      ArrayList<Policy> policies = new ArrayList<Policy>();
+      //class instance declarations
       
       int policyNum;
       int age;
@@ -16,68 +22,42 @@ public class Project_jonathan_stroz
       String smoker;
       //user-defined variables
       
-      System.out.print("Please enter the Policy Number: ");
-      policyNum = keyboard.nextInt();
-      keyboard.nextLine();
-      //user enters policy number
-      
-      System.out.print("Please enter the Provider Name: ");
-      providerName = keyboard.nextLine();
-      //user enters policy provider name
-      
-      System.out.print("Please enter the Policyholder's First Name: ");
-      nameFirst = keyboard.nextLine();
-      //user enters policyholder's first name
-      
-      System.out.print("Please enter the Policyholder's Last Name: ");
-      nameLast = keyboard.nextLine();
-      //user enters policyholder's last name
-      
-      System.out.print("Please enter the Policyholder's Age: ");
-      age = keyboard.nextInt();
-      keyboard.nextLine();
-      //user enters policyholder's age
-      
-      System.out.print("Please enter the Policyholder's Smoking Status (smoker/non-smoker): ");
-      smoker = keyboard.nextLine();
-      while(!smoker.equalsIgnoreCase("smoker") && !smoker.equalsIgnoreCase("non-smoker"))
+      do
       {
-         System.out.print("\nInvalid input.");
-         System.out.print("\nPlease enter the Policyholder's Smoking Status (smoker/non-smoker): ");
-         smoker = keyboard.nextLine();
+         policyNum = fileRead.nextInt();
+         fileRead.nextLine();
+         //policy number is read
+         
+         providerName = fileRead.nextLine();
+         //policy provider name is read
+         
+         nameFirst = fileRead.nextLine();
+         //policyholder's first name is read
+         
+         nameLast = fileRead.nextLine();
+         //policyholder's last name is read
+         
+         age = fileRead.nextInt();
+         fileRead.nextLine();
+         //policyholder's age is read
+         
+         smoker = fileRead.nextLine();
+         //policyholder's smoking status is read
+         
+         height = fileRead.nextDouble();
+         fileRead.nextLine();
+         //policyholder's height is read
+         
+         weight = fileRead.nextDouble();
+         //policyholder's weight is read
+         
+         policies.add(new Policy(policyNum, providerName, nameFirst, nameLast, age, smoker, height, weight));
+         //new Policy class instance constructed
       }
-      if(smoker.equalsIgnoreCase("smoker"))
-      {
-         smoker = "smoker";
-      }
-      else
-      {
-         smoker = "non-smoker";
-      }
-      //user enters policyholder's smoking status
+      while(fileRead.hasNext());
+      //policy information is read from file
       
-      System.out.print("Please enter the Policyholder's Height (in inches): ");
-      height = keyboard.nextDouble();
-      keyboard.nextLine();
-      //user enters policyholder's height
-      
-      System.out.print("Please enter the Policyholder's Weight (in pounds): ");
-      weight = keyboard.nextDouble();
-      keyboard.nextLine();
-      //user enters policyholder's weight
-      
-      Policy account = new Policy(policyNum, providerName, nameFirst, nameLast, age, smoker, height, weight);
-      //new Policy class instance constructed
-      
-      System.out.print("\nPolicy Number: " + account.getPolicyNum());
-      System.out.print("\nProvider Name: " + account.getProviderName());
-      System.out.print("\nPolicyholder's First Name: " + account.getFirstName());
-      System.out.print("\nPolicyholder's Last Name: " + account.getLastName());
-      System.out.print("\nPolicyholder's Age: " + account.getAge());
-      System.out.print("\nPolicyholder's Smoking Status: " + account.getSmokingStatus());
-      System.out.print("\nPolicyholder's Height: " + account.getHeight() + " inches");
-      System.out.print("\nPolicyholder's Weight: " + account.getWeight() + " pounds");
-      System.out.printf("\nPolicyholder's BMI: %.2f", account.calculateBMI());
-      System.out.printf("\nPolicy Price: $%.2f", account.calculateInsurancePrice());
+      fileRead.close();
+      //file that was being read is closed
    }
 }
